5a7b2c813863532b4ca82d01442ee86a1b772948
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-16 15:50:04 +0900

    Initial Commit

---
+++ Project9Paer2/Project9Paer2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project9Paer2App.swift
+//  Project9Paer2
+//
+//  Created by 山本響 on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct Project9Paer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
